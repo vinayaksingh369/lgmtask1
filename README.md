d95bb1e94327aac7af1edd18ef74a4e0c1840898
“[firstname <vinayaksingh2172001@gmail.com>
2021-10-27 16:17:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LgmTask1
+
+View My website -: https://himanshu631.github.io/LgmTask1/
---
+++ index.js
@@ -0,0 +1,44 @@
+// function imgslider(links){
+//     document.querySelector(".slider").src = links;
+// }
+
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbarres') 
+navlist = document.querySelector('.nav-list') 
+
+
+burger.addEventListener('click',()=>{
+    
+    navlist.classList.toggle('v-class');
+    navbar.classList.toggle('h-nav');
+     
+})
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
